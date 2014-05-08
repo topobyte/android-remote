d3ef38ba5cadb13da01d11fa29f04a9737304b15
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2014-05-08 12:32:30 +0200

    Initial project setup

---
+++ project/src/de/topobyte/androidremote/Viewer.java
@@ -0,0 +1,26 @@
+// Copyright 2014 Sebastian Kuerten
+//
+// This file is part of android-remote.
+//
+// android-remote is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// android-remote is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with android-remote. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.androidremote;
+
+public class Viewer
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
